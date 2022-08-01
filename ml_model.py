90460bd6edfa5c23df49c476af186f610f89937b
varalasravan <skreddy.varala@gmail.com>
2022-08-01 15:41:45 -0700

    added 3 Ml model codes

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ML related code here")
+
+def model1():
+    print("Code for Model 1")
+
+def model2():
+    print("Code for Model 2")
+
+def model3():
+    print("Code for Model 3")
